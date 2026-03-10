13b545e687143894f1a069d96bf7feb0e822821f
elelelwelwelwel <elelelwelwelwel@gmail.com>
2026-03-10 10:30:56 +0700

    Menambahkan script untuk nama

---
+++ Assets/Scripts/TambahNamaSamuel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TambahNamaSamuel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("nama saya elel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
